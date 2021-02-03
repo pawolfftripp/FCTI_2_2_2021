6c992721e7c6f7145e58a0860327a9765039cdbf
haswell <perry.wolfftripp@gmail.com>
2021-02-03 08:06:01 -0500

    First Commit

---
+++ PowerArray.java
@@ -0,0 +1,11 @@
+public class PowerArray {
+
+
+    public static int[] toPower(int size, int power) {
+
+        int[] array1 = {};
+        System.out.println("What");
+        return array1;
+
+    }
+}
